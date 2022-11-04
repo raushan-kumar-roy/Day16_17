3bcf81349d63e632cab1577bf35f611f7cde5746
raushan-kumar-roy <114593668+raushan-kumar-roy@users.noreply.github.com>
2022-11-04 11:28:00 +0530

    anagram

---
+++ anagram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSprogram
+{
+    public class anagram
+    {
+        public void Display()
+        {
+            string str1 = "heater";
+            string str2 = "reheat";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Anagram");
+            }
+            else
+            {
+                Console.WriteLine("Not Anagram");
+            }
+        }
+    }
+}
